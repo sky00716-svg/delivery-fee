1dd0872bdbe25f3a47df664a166dc3d41eb861fe
sky00716-svg <sky00716@gmail.com>
2026-08-10 23:30:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='delivery-fee-v1';
+const ASSETS=['./','./index.html','./manifest.json','./sw.js','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
